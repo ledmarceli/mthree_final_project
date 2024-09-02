a862499562b148fb825f4b0910c2efb4a2626781
ledmarceli <ledmarceli.64@gmail.com>
2024-09-02 16:47:33 +0200

    Added first and last name

--- python_scripts/app.py
+++ python_scripts/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 
 app = Flask(__name__)
 
@@ -9,6 +9,8 @@ def index():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     try:
+        first_name = escape(request.form["first_name"])
+        last_name = escape(request.form["last_name"])
         gross_income = float(request.form['income'])
         expenses = float(request.form['expenses'])
         net_income = gross_income-expenses
@@ -21,7 +23,7 @@ def calculate():
         else:
             tax = (50270-12750)*0.2+(125140-50270)*0.4+(net_income-125140)*0.45
         
-        return render_template('result.html', income=gross_income, expenses=expenses, tax=tax)
+        return render_template('result.html', income=gross_income, expenses=expenses, tax=tax, first_name=first_name, last_name=last_name)
     except ValueError:
         return "Please enter a valid number for income."
 
